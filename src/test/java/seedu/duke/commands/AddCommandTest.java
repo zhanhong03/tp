62358e982c9bd42d981257a5b57ec75937b0a227
wjw55 <wjw50100@gmail.com>
2026-03-12 17:20:58 +0800

    Resolve style issue

--- src/test/java/seedu/duke/commands/AddCommandTest.java
+++ src/test/java/seedu/duke/commands/AddCommandTest.java
@@ -1,6 +1,6 @@
 package seedu.duke.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
